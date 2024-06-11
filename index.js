c1c4f7cb91257d138ced302044c249118936b3ea
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2024-06-11 23:46:18 +0530

    session fix

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// Server Using Express
 import express from "express";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import passport from "passport";
 import bodyParser from "body-parser";
-import cors from "cors"
+import cors from "cors";
 
 import "./env.js";
 import mongooseConnectToDB from "./src/config/mongooseConfig.js";
 import router from "./routes.js";
+
 const server = express();
 
 const corsOptions = {
@@ -16,7 +17,7 @@ const corsOptions = {
   credentials: true, // Allow credentials
 };
 
-server.use(cors(corsOptions))
+server.use(cors(corsOptions));
 
 const port = process.env.PORT || 3622;
 const hostname = process.env.HOST_NAME;
@@ -25,25 +26,33 @@ server.use(express.json());
 server.use(bodyParser.json());
 server.use(express.urlencoded({ extended: true }));
 
+// MongoDB URI
+const mongoUri = process.env.URL;
+
 // Middleware
 server.use(
   session({
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false } 
+    store: MongoStore.create({
+      mongoUrl: mongoUri,
+      collectionName: 'sessions'
+    }),
+    cookie: { secure: true } // Set to true if using HTTPS
   })
 );
+
 server.use(passport.initialize());
 server.use(passport.session());
 
-// router parent middleware;
+// Router parent middleware
 server.use(router);
 
-server.listen(port, () => {
+server.listen(port, async () => {
   try {
     console.log(`Server is running at ${port}`);
-    mongooseConnectToDB();
+    await mongooseConnectToDB();
   } catch (error) {
     console.error("Error while connecting to database", error);
   }
